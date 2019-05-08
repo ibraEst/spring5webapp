6e1eda04d496e4c8fa95d6427ea7bc6a553e51f7
benlazreg.i <benlazreg.i@sfeir.com>
2019-05-08 14:46:58 +0200

    add my JPA entities

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5webapp.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<Book>();
+
+}
